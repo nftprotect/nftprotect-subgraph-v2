b8aa0d5cc5d8938086a889eec69792a987421c42
developer <i.svirin@nordavind.ru>
2023-08-26 20:56:11 +0000

    Implement indexer for UserRegistry contract

--- src/user-registry.ts
+++ src/user-registry.ts
@@ -1,5 +1,10 @@
 import
 {
+    store
+} from "@graphprotocol/graph-ts"
+
+import
+{
     AffiliatePayment as AffiliatePaymentEvent,
     AffiliatePercentChanged as AffiliatePercentChangedEvent,
     ArbitratorRegistryChanged as ArbitratorRegistryChangedEvent,
@@ -17,33 +22,133 @@ import
     SuccessorRequested as SuccessorRequestedEvent
 } from "../generated/UserRegistry/UserRegistry"
 
+import
+{
+    System,
+    Partner,
+    DID,
+    SuccessorRequest
+} from "../generated/schema"
 
-export function handleAffiliatePayment(event: AffiliatePaymentEvent): void{}
+import
+{
+    loadSystem
+} from "./system"
 
-export function handleAffiliatePercentChanged(event: AffiliatePercentChangedEvent): void {}
+import
+{
+    loadUser
+} from "./user"
 
-export function handleArbitratorRegistryChanged(event: ArbitratorRegistryChangedEvent): void {}
 
-export function handleDIDRegistered(event: DIDRegisteredEvent): void {}
+export function handleAffiliatePayment(event: AffiliatePaymentEvent): void
+{
+    // do nothing
+}
 
-export function handleDIDUnregistered(event: DIDUnregisteredEvent): void {}
+export function handleAffiliatePercentChanged(event: AffiliatePercentChangedEvent): void
+{
+    const s = loadSystem("userregistry");
+    s.affiliatePercent = event.params.percent;
+    s.save();
+}
 
-export function handleDeployed(event: DeployedEvent): void {}
+export function handleArbitratorRegistryChanged(event: ArbitratorRegistryChangedEvent): void
+{
+    // do nothing
+}
 
-export function handleFeeChanged(event: FeeChangedEvent): void {}
+export function handleDIDRegistered(event: DIDRegisteredEvent): void
+{
+    let d = new DID(event.params.did.toHex().toString());
+    d.provider = event.params.provider;
+    d.save();
+}
 
-export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {}
+export function handleDIDUnregistered(event: DIDUnregisteredEvent): void
+{
+    store.remove("DID", event.params.did.toHex().toString());
+}
 
-export function handlePartnerSet(event: PartnerSetEvent): void {}
+export function handleDeployed(event: DeployedEvent): void
+{
+    const s = loadSystem("userregistry");
+}
 
-export function handleReferrerSet(event: ReferrerSetEvent): void {}
+export function handleFeeChanged(event: FeeChangedEvent): void
+{
+    const s = loadSystem("userregistry");
+    if(event.params.pr == 0)
+    {
+        s.feeBasic = event.params.feeWei;
+    }
+    else
+    {
+        s.feeUltra = event.params.feeWei;
+    }
+    s.save();
+}
 
-export function handleRewardedCouponsChanged(event: RewardedCouponsChangedEvent): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void
+{
+    // do nothing
+}
 
-export function handleScoreThresholdChanged(event: ScoreThresholdChangedEvent): void {}
+export function handlePartnerSet(event: PartnerSetEvent): void
+{
+    let p = new Partner(event.params.partnet.toHex().toString());
+    p.percent = event.params.percent;
+    p.save();
+}
 
-export function handleSuccessorApproved(event: SuccessorApprovedEvent): void {}
+export function handleReferrerSet(event: ReferrerSetEvent): void
+{
+    let u = loadUser(event.params.user);
+    u.referrer = event.params.referrer;
+    u.save();
+}
 
-export function handleSuccessorRejected(event: SuccessorRejectedEvent): void {}
+export function handleRewardedCouponsChanged(event: RewardedCouponsChangedEvent): void
+{
+    // do nothing
+}
 
-export function handleSuccessorRequested(event: SuccessorRequestedEvent): void {}
+export function handleScoreThresholdChanged(event: ScoreThresholdChangedEvent): void
+{
+    const s = loadSystem("userregistry");
+    s.scoreThreshold = event.params.threshold;
+    s.save();
+}
+
+export function handleSuccessorApproved(event: SuccessorApprovedEvent): void
+{
+    let r = SuccessorRequest.load(event.params.requestId.toString()) as SuccessorRequest;
+    r.status = "Accepted";
+    r.timestampChange = event.block.timestamp;
+    r.blocknumberChange = event.block.number;
+    r.save();
+
+    let u = loadUser(r.user);
+    u.successor = r.successor;
+    u.save();
+}
+
+export function handleSuccessorRejected(event: SuccessorRejectedEvent): void
+{
+    let r = SuccessorRequest.load(event.params.requestId.toString()) as SuccessorRequest;
+    r.status = "Rejected";
+    r.timestampChange = event.block.timestamp;
+    r.blocknumberChange = event.block.number;
+    r.save();
+}
+
+export function handleSuccessorRequested(event: SuccessorRequestedEvent): void
+{
+    let r = new SuccessorRequest(event.params.requestId.toString());
+    r.user = event.params.user;
+    r.successor = event.params.successor;
+    r.status = "InDispute";
+    r.timestamp = event.block.timestamp;
+    r.blocknumber = event.block.number;
+    r.save();
+}
