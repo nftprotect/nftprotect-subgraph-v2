01b9402eda4ea3705039c9a524be45d8126beb08
developer <i.svirin@nordavind.ru>
2023-08-27 19:47:34 +0000

    Implement Requests indexer

--- src/request-hub.ts
+++ src/request-hub.ts
@@ -1,5 +1,10 @@
 import
 {
+    BigInt
+} from "@graphprotocol/graph-ts"
+
+import
+{
     ArbitratorRegistryChanged as ArbitratorRegistryChangedEvent,
     BurnAnswered as BurnAnsweredEvent,
     BurnArbitrateAsked as BurnArbitrateAskedEvent,
@@ -17,6 +22,11 @@ import
 
 import
 {
+    Request
+} from "../generated/schema"
+
+import
+{
     loadSystem
 } from "./system"
 
@@ -26,32 +36,107 @@ import
 } from "./user"
 
 
-export function handleArbitratorRegistryChanged(event: ArbitratorRegistryChangedEvent): void {}
+function closeDispute(requestId: BigInt, accept: boolean, timestamp: BigInt, blocknumber: BigInt): void
+{
+    let r = Request.load(requestId.toString()) as Request;
+    if (r.status == "Initial")
+    {
+        r.status = accept ? "Accepted" : "Rejected";
+    }
+    else
+    {
+        r.status = accept ? "DisputeAccepted" : "DisputeRejected";
+    }
+    r.timestampChange = timestamp;
+    r.blocknumberChange = blocknumber;
+    r.save();
+}
 
-export function handleBurnAnswered(event: BurnAnsweredEvent): void {}
+export function handleArbitratorRegistryChanged(event: ArbitratorRegistryChangedEvent): void
+{
+    // do nothing
+}
 
-export function handleBurnArbitrateAsked(event: BurnArbitrateAskedEvent): void {}
+export function handleBurnAnswered(event: BurnAnsweredEvent): void
+{
+    closeDispute(event.params.requestId, event.params.accept, event.block.timestamp, event.block.number);
+}
+
+export function handleBurnArbitrateAsked(event: BurnArbitrateAskedEvent): void
+{
+    let r = new Request(event.params.requestId.toString());
+    r.type = "Burn";
+    r.status = "Disputed";
+    r.entities = [];
+    r.newowner = loadUser(event.params.dst).id;
+    r.timestamp = event.block.timestamp;
+    r.blocknumber = event.block.number;
+    r.save();
+}
 
 export function handleDeployed(event: DeployedEvent): void
 {
     const s = loadSystem("requesthub");
 }
 
-export function handleDisputeCreated(event: DisputeCreatedEvent): void {}
+export function handleDisputeCreated(event: DisputeCreatedEvent): void
+{
+    let r = Request.load(event.params.requestId.toString()) as Request;
+    r.arbitrator = event.params.arbitratorId.toString();
+    r.disputeId = event.params.disputeId;
+    r.save();
+}
 
-export function handleEntityAddedToRequest(event: EntityAddedToRequestEvent): void {}
+export function handleEntityAddedToRequest(event: EntityAddedToRequestEvent): void
+{
+    let r = Request.load(event.params.requestId.toString()) as Request;
+    r.entities.push(event.params.entityId.toString());
+    r.save();
+}
 
-export function handleOwnershipAdjusted(event: OwnershipAdjustedEvent): void {}
+export function handleOwnershipAdjusted(event: OwnershipAdjustedEvent): void
+{
+    // do nothing
+}
 
-export function handleOwnershipAdjustmentAnswered(event: OwnershipAdjustmentAnsweredEvent): void {}
+export function handleOwnershipAdjustmentAnswered(event: OwnershipAdjustmentAnsweredEvent): void
+{
+    closeDispute(event.params.requestId, event.params.accept, event.block.timestamp, event.block.number);
+}
 
-export function handleOwnershipAdjustmentArbitrateAsked(event: OwnershipAdjustmentArbitrateAskedEvent): void {}
+export function handleOwnershipAdjustmentArbitrateAsked(event: OwnershipAdjustmentArbitrateAskedEvent): void
+{
+    // do nothing
+}
 
-export function handleOwnershipAdjustmentAsked(event: OwnershipAdjustmentAskedEvent): void {}
+export function handleOwnershipAdjustmentAsked(event: OwnershipAdjustmentAskedEvent): void
+{
+    let r = new Request(event.params.requestId.toString());
+    r.type = "OwnershipAdjustment";
+    r.status = "Initial";
+    r.entities = [];
+    r.newowner = loadUser(event.params.dst).id;
+    r.timestamp = event.block.timestamp;
+    r.blocknumber = event.block.number;
+    r.save();
+}
 
-export function handleOwnershipRestoreAnswered(event: OwnershipRestoreAnsweredEvent): void {}
+export function handleOwnershipRestoreAnswered(event: OwnershipRestoreAnsweredEvent): void
+{
+    closeDispute(event.params.requestId, event.params.accept, event.block.timestamp, event.block.number);
+}
 
-export function handleOwnershipRestoreAsked(event: OwnershipRestoreAskedEvent): void {}
+export function handleOwnershipRestoreAsked(event: OwnershipRestoreAskedEvent): void
+{
+    let r = new Request(event.params.requestId.toString());
+    r.type = "OwnershipRestore";
+    r.status = "Disputed";
+    r.entities = [];
+    r.newowner = loadUser(event.params.newowner).id;
+    r.timestamp = event.block.timestamp;
+    r.blocknumber = event.block.number;
+    r.save();
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void
 {
